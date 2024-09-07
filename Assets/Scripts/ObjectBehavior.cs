628db8cdba57642598f7b1ed5376e4ba27354535
Dylan Torres <glaciershield@gmail.com>
2024-09-06 22:55:11 -0400

    Object Scale Field and Enable/Disable Button In Progress
    Some warning messages for the Object Scale now display as intended, but they do not yet affect the actual scale of the object. The Enable/Disable button currently selects all objects rather than the designated type, and also still needs to change color.

--- Assets/Scripts/ObjectBehavior.cs
+++ Assets/Scripts/ObjectBehavior.cs
@@ -1,25 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
+
 
 
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 using static UnityEngine.GraphicsBuffer;
 
-
+public enum ObjectType { Cube, Sphere };
 
 public class ObjectBehavior : MonoBehaviour
 {
 
     public ObjectType objectType;
+    public float objectScale; // TO DO: Hide this value in the inspector
 
 }
-
-public enum ObjectType { Cube, Sphere };
 
 #if UNITY_EDITOR
 
@@ -27,26 +29,22 @@ public enum ObjectType { Cube, Sphere };
 [CanEditMultipleObjects]
 public class ObjectEditor : Editor
 {
-
    
-
     public override void OnInspectorGUI()
-    {
+    {        
 
         base.OnInspectorGUI();
         ObjectType editorType = (target as ObjectBehavior).objectType;
 
         EditorGUILayout.BeginHorizontal();
         if (GUILayout.Button("Select All"))
-        {
-           
+        {           
             var ourBehaviour = GameObject.FindObjectsOfType<ObjectBehavior>();
 
             var ourObjects = ourBehaviour
                 .Select(theObject => { if (theObject.objectType == editorType) { return theObject.gameObject; } else { return null; } })
                 .ToArray();
             Selection.objects = ourObjects;
-
 
             Debug.Log("This button works!");
         }
@@ -57,10 +55,36 @@ public class ObjectEditor : Editor
         }
         EditorGUILayout.EndHorizontal();
 
-        if (GUILayout.Button("Disable/Enable objects", GUILayout.Height(40)))
+        if (GUILayout.Button("Disable/Enable Objects", GUILayout.Height(40)))
         {
+            // TO DO: This should select each of one object type, but it currently selects all the objects. 
+            // It should also change between colors when clicked.
+            foreach (var typeOfObject in GameObject.FindObjectsOfType<ObjectBehavior>(true) ) 
+            {
+                typeOfObject.gameObject.SetActive(!typeOfObject.gameObject.activeSelf);
+            }
             
         }
+
+        float minValue = 0.5f, maxValue = 3.0f;
+
+        serializedObject.Update();
+        var thisObjectScale = serializedObject.FindProperty("objectScale");
+        EditorGUILayout.PropertyField(thisObjectScale);       
+
+        if (thisObjectScale.floatValue <= minValue)
+        {
+            EditorGUILayout.HelpBox("This object's minimum scale is " + minValue + "!", MessageType.Warning);
+            thisObjectScale.floatValue = minValue;
+        }
+        else if (thisObjectScale.floatValue >= maxValue) 
+        {
+            EditorGUILayout.HelpBox("This object's maximum scale is " + maxValue + "!", MessageType.Warning);
+            thisObjectScale.floatValue = maxValue;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+
     }
 
     // Start is called before the first frame update
